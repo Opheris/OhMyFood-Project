ee15f5f3e05e9aaa03dd62b0ae267df37b7ccb89
Heddi <heddi@example.com>
2025-11-22 02:30:14 +0100

    chore: publish latest site changes

--- compile.ps1
+++
@@ -1,8 +0,0 @@
-# Script de compilation SCSS
-sass --no-source-map scss/restaurant.scss style/restaurant.css
-
-# Nettoyer les fichiers parasites dans scss/
-Remove-Item "scss/restaurant.css" -Force -ErrorAction SilentlyContinue
-Remove-Item "scss/restaurant.css.map" -Force -ErrorAction SilentlyContinue
-
-Write-Host "✅ Compilation terminée et nettoyée !" -ForegroundColor Green
